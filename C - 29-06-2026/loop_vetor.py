96acdab8e0a0d86b1a28e41019f19d7cecf27761
LEONARDO DE OLIVEIRA JULIAO <leo_rbd.raki@hotmail.com>
2026-06-30 20:29:13 -0300

    Adicionando arquivo sobre loop com vetores

---
+++ C - 29-06-2026/loop_vetor.py
@@ -0,0 +1,3 @@
+paises = ["Brasil", "Japão", "Butão", "Noruega", "Cabo Verde", "Costa do Marfim", "México", "Bélgica", "Argentina", "França,", "Paraguai", "Equador"]
+
+#! O loop funciona muito bem com vetores
